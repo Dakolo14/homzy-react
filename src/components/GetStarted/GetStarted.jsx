403ec1adff19719e13b671ba3e8571ee053c6b0e
Dakolo14 <ajosedare4u@gmail.com>
2024-01-22 14:31:57 +0100

    first commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GetStarted.css'
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+        
+    </section>
+  )
+}
+
+export default GetStarted
