aa5877cb267bf8f25e80cb8f71a4e186e8863e6a
Dakolo14 <ajosedare4u@gmail.com>
2024-01-23 15:29:35 +0100

    first commit

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -4,7 +4,15 @@ import './GetStarted.css'
 const GetStarted = () => {
   return (
     <section className="g-wrapper">
-        
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started with Homzy</span>
+                <span className='secondaryText'>Subscribe and find super attractive price quotes from us <br /> Find your residence soon</span>
+                <button className="button">
+                    <a href="mailto:ajosedare4u@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
     </section>
   )
 }
